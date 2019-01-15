473241a7b4d7a33ead6a722da8f5cc6538d733d9
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 12:07:50 +0530

    Initial commit

---
+++ Random6digits10times.py
@@ -0,0 +1,4 @@
+""" This Program again why i wrote , for getting 6digits random numbers 10times"""
+from random import*
+for i in range(10):
+    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
